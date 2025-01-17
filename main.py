e072b99551164879bdf4c580b0cc36d144c22bf2
blink515 <bl_blink@protonmail.com>
2025-01-17 06:08:19 -0600

    Added asteroids and asteroidfield. Created update groups

--- main.py
+++ main.py
@@ -1,29 +1,46 @@
 import pygame
+from asteroidfield import AsteroidField
 from constants import *
-from player import *
+from player import Player
+from asteroid import Asteroid
+
 
 def main():
-	pygame.init()
-	clock = pygame.time.Clock()
-	dt = 0
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
 
-	print("Starting asteroids!")
-	print(f"Screen width: {SCREEN_WIDTH}")
-	print(f"Screen height: {SCREEN_HEIGHT}")
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
-	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-	player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
 
-	while True:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				return
-		screen.fill((0,0,0))
-		player.draw(screen)
-		player.update(dt)
-		pygame.display.flip()
-		dt = clock.tick(60) / 1000
-		
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
+
+    dt = 0
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        for obj in updatable:
+            obj.update(dt)
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
-	main()
+    main()
