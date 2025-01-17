5b1d178f9bf851a2b179cade214b98423af2d90f
blink515 <bl_blink@protonmail.com>
2025-01-16 23:05:24 -0600

    Add 60 fps lock

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from constants import *
 
 def main():
 	pygame.init()
+	clock = pygame.time.Clock()
+	dt = 0
+
 	print("Starting asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
@@ -13,12 +16,10 @@ def main():
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
-		game(screen)
-
-def game(screen):
-
-	screen.fill((0,0,0))
-	pygame.display.flip()
+		screen.fill((0,0,0))
+		pygame.display.flip()
+		dt = clock.tick(60) / 1000
+		
 
 if __name__ == "__main__":
 	main()
